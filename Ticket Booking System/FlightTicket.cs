a11032b5cbc7982781a0347c587e84f4e8d90ac8
Anik927 <mirzaanik927@gmail.com>
2025-11-02 18:51:13 +0600

    Day-6

---
+++ Ticket Booking System/FlightTicket.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket_Booking_System
+{
+    internal class FlightTicket:Ticket
+    {
+        private string seatClass;
+        private double luggageWeight;
+
+        public string SeatClass
+        {
+            get { return seatClass; }
+            set { seatClass = value; }
+        }
+
+        public double LuggageWeight
+        {
+            get { return luggageWeight; }
+            set { luggageWeight = value; }
+        }
+
+        public override void ShowDetails()
+        {
+            base.ShowDetails();
+            Console.WriteLine(seatClass);
+            Console.WriteLine(luggageWeight);
+        }
+
+        public bool IsExcessLuggage()
+        {
+            return luggageWeight > 20;
+        }
+
+        public bool IsPrioritySeat()
+        {
+            return seatClass == "Business";
+        }
+
+        public bool IsPrioritySeat(bool isFrequentFlyer)
+        {
+            if (isFrequentFlyer == true && seatClass == "Business")
+                return true;
+            else 
+                return false;
+        }
+
+        public FlightTicket()
+        {
+
+        }
+
+        public FlightTicket(int ticketId, string passengerName, string seatClass, double luggageWeight):base(ticketId, passengerName)
+        {
+            this.seatClass = seatClass;
+            this.luggageWeight= luggageWeight;
+        }
+
+
+    }
+}
