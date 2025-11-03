270e98153629b5afa35a1f0bf06c89677b145b99
Anik927 <mirzaanik927@gmail.com>
2025-11-04 03:12:09 +0600

    Day-7

---
+++ Online Payment System/CreditCardPayment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Payment_System
+{
+    internal class CreditCardPayment:Payment
+    {
+        public override bool Validate()
+        {
+            Console.Write("Enter Your Card Number: ");
+            string cardNum = Console.ReadLine();
+            Console.Write("Enter Your Card CVV: ");
+            string cardCVV = Console.ReadLine();
+
+            return cardNum.Length == 16 && cardCVV.Length == 3;
+
+        }
+
+        public override bool ProcessPayment(double amount)
+        {
+            return amount>=0 && transactionId>0;
+        }
+
+    }
+}
---
+++ Online Payment System/Payment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Payment_System
+{
+    internal abstract class Payment : IPayable
+    {
+        protected double amount;
+        protected int transactionId;
+
+        public double Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        public int TransactionId
+        {
+            get { return transactionId; }
+            set { transactionId = value; }
+        }
+
+        public Payment() { }
+
+        public Payment(double amount, int transactionId)
+        {
+            this.amount = amount;
+            this.transactionId = transactionId;
+        }
+
+        public abstract bool ProcessPayment(double amount);
+
+        public abstract bool Validate();
+
+    }
+}
