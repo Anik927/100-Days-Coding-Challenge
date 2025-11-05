19cc74caa90786bfd497efbcb236da878f573ce2
Anik927 <mirzaanik927@gmail.com>
2025-11-05 18:28:19 +0600

    Day-8

---
+++ Student Activities/DramaClub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Activities
+{
+    internal class DramaClub
+    {
+
+        private string playTitle;
+        private string role;
+        
+        public string PlatTitle
+        {
+            get { return playTitle; } 
+            set { playTitle = value; }
+        }
+
+        public string Role
+        {
+            get { return role; }
+            set { role = value; }
+        }
+
+        public DramaClub() { }
+
+        public DramaClub(string playTitle, string role)
+        {
+            this.playTitle = playTitle;        
+            this.role = role;
+        }
+
+        public void Participate()
+        {
+            Console.WriteLine($"Acting as {role} in {playTitle}.");
+        }
+
+    }
+}
---
+++ Student Activities/Sports.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Activities
+{
+    internal class Sports
+    {
+
+        private string sportName;
+        private int teamSize;
+
+        public string SportName
+        {
+            get { return sportName; }
+            set { sportName = value; }
+        }
+
+        public int TeamSize
+        {
+            get { return teamSize; }
+            set { teamSize = value; }
+        }
+
+        public Sports() { }
+
+        public Sports(string sportName, int teamSize)
+        {
+            this.sportName = sportName;
+            this.teamSize = teamSize;
+        }
+
+        public void Participate()
+        {
+            Console.WriteLine($"Playing {sportName} in a team of {teamSize}.")
+        }
+
+    }
+}
