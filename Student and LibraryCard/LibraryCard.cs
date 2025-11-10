a151dfb4afcb35a347aabce4754a446c3e00ec83
Anik927 <mirzaanik927@gmail.com>
2025-11-10 21:31:10 +0600

    Day-10

---
+++ Student and LibraryCard/LibraryCard.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_and_LibraryCard
+{
+    internal class LibraryCard
+    {
+
+        private string cardNumber;
+        private DateTime issueDate;
+        private bool isActive;
+
+        public string CardNumber
+        {
+            get { return cardNumber; } 
+            set {  cardNumber = value; }
+        }
+
+        public DateTime IssueDate
+        { 
+            get { return issueDate; } 
+            set { issueDate = value; } 
+        }
+
+        public bool IsActive
+        {
+            get { return isActive; } 
+            set {  isActive = value; }
+        }
+
+        public LibraryCard() { }
+
+        public LibraryCard(string cardNumber, DateTime issueDate, bool isActive)
+        {
+            this.cardNumber = cardNumber;
+            this.issueDate = issueDate;
+            this.isActive = isActive;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Card Number: {cardNumber}");
+            Console.WriteLine($"Issue Date: {issueDate}");
+            Console.WriteLine($"Is Active: {issueDate}");
+        }
+
+
+
+    }
+}
---
+++ Student and LibraryCard/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_and_LibraryCard
+{
+    internal class Student
+    {
+
+        private int studentID;
+        private string name;
+        private LibraryCard libraryCard;
+
+        public int StudentID
+        {
+            get { return studentID; }
+            set { studentID = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public LibraryCard LibraryCard
+        {
+            get { return libraryCard; }
+            set { libraryCard = value; }
+        }
+
+        public Student() { }
+
+        public Student(int studentID, string name, LibraryCard libraryCard)
+        {
+            this.studentID = studentID;
+            this.name = name;
+            this.libraryCard = libraryCard;
+        }
+
+        public void ActiveLibaryCard()
+        {
+            libraryCard.IsActive = true;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Student Details: ");
+            Console.WriteLine($"Student ID: {studentID}");
+            Console.WriteLine($"Name : {name}");
+            Console.WriteLine("LibaryCard Details: ");
+            libraryCard.ShowDetails();
+        }
+        
+    }
+}
